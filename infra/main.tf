605144981bbf37f98ef7b3041d90b14f02f9a3c2
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 21:10:04 +0000

    uniform_bucket_level_access updated

--- infra/main.tf
+++ infra/main.tf
@@ -5,4 +5,29 @@ resource "random_id" "default" {
 resource "google_storage_bucket" "default" {
   name                        = "${random_id.default.hex}-gcf-source"
   location                    = "europe-west2"
+  force_destroy = true
+
+  uniform_bucket_level_access {
+    enabled = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365
+    }
+  }
+
+  cors {
+    origin = ["http://example.com"]
+    method = ["GET", "HEAD"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
 }
