3cdabf3822ae687fd157eaeae1f2c27bb7aaedfd
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 17:23:41 +0000

    Initial draft.

---
+++ infra/main.tf
@@ -0,0 +1,8 @@
+resource "random_id" "default" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.default.hex}-gcf-source"
+  location                    = "europe"
+}
---
+++ infra/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.16.0"
+    }
+  }
+  backend "gcs" { 
+    bucket = "pocapps-tfstate" 
+    prefix = "terraform/state" 
+    project = "pocapps" 
+  }
+}
+
+provider "google" {
+  project = "pocapps" 
+  region = "europe-west2"
+}
