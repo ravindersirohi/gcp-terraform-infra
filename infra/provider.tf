f74aa6757b96c3cf8a800692eb876707558fbec4
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 23:24:13 +0000

    workspace added

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "gcs" { 
     bucket = "pocapps-tfstate" 
-    prefix = "terraform/state/gpc-terraform-infra"
+    prefix = "terraform/state/gpc-terraform-infra-${terraform.workspace}"
   }
 }
 
