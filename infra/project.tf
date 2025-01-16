fef3fcc1d09785efa5d8a244cd09b7ddf4b13c62
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-16 16:27:46 +0000

    folder takne off

--- infra/project.tf
+++ infra/project.tf
@@ -1,13 +1,14 @@
 
 data "google_project" "default" { }
 
-resource "google_folder" "devops" {
-  display_name = "sirohi-devops-${terraform.workspace}"
-  parent = data.google_project.default.org_id
-}
+# resource "google_folder" "devops" {
+#   display_name = "sirohi-devops-${terraform.workspace}"
+#   parent = data.google_project.default.org_id
+# }
 
 resource "google_project" "current" {
-  name          = var.project.name
-  project_id    = var.project.id
-  folder_id     = google_folder.devops.name
+    name          = var.project.name
+    project_id    = var.project.id
+    org_id = data.google_project.default.org_id
+    #   folder_id     = google_folder.devops.name
 }
